31a684194d91c002a1aaa265ba6bff7da6ccbf4f
Colin Moore <cmoore1711@gmail.com>
2018-06-26 13:39:15 -0400

    Update restartCode.py
    Edited for DT ToolTrackerTouchscreen

--- DataAcquisition/PiDataAcquisition/restartCode.py
+++ DataAcquisition/PiDataAcquisition/restartCode.py
@@ -4,28 +4,23 @@ import psutil
 
 
 
-isPiDaqCodeRunning = False
+
 isTimeKeeperRunning = False
 
 
 
 for pid in psutil.pids():
     p = psutil.Process(pid)
-    if(p.name() == "python3" and len(p.cmdline()) >1 and "piDaqCode.py" in p.cmdline()[1]):
-        isPiDaqCodeRunning = True
     if(p.name() == "python3" and len(p.cmdline()) >1 and "pythonTimeSync.py" in p.cmdline()[1]):
         isTimeKeeperRunning = True
         #print(p.cmdline()[1])
         #
-print(isPiDaqCodeRunning)
+
 print(isTimeKeeperRunning)
 
 
-os.system("cd /home/pi/Desktop/PiDataAcquisition && sudo git pull")
+os.system("cd /home/pi/Desktop/DiamondTurning && sudo git pull")
 
-if(not isPiDaqCodeRunning):
-
-    os.system("cd && python3 Desktop/PiDataAcquisition/PiDataAcquisition/piDaqCode.py")
 if(not isTimeKeeperRunning):
     os.system("cd && python3 Desktop/PiDataAcquisition/PiDataAcquisition/pythonTimeSync.py")
 
